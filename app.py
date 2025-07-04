52f50375901e0e8ad97e8028b92ef44d50bb59d1
Angel <anngel.lopez01@gmail.com>
2025-07-04 00:29:06 -0600

    Actualizacion

--- app.py
+++ app.py
@@ -101,7 +101,9 @@ def registrar():
 
 @app.route('/prestamos')
 def prestamos():
-    return render_template('prestamos.html')
+    libro_id = request.args.get('id')
+    return render_template('prestamos.html', libro_id=libro_id)
+
 
 @app.route('/devolver', methods=['POST'])
 def devolver_libro():
